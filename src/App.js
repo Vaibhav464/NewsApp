415f801eeec71c7a80e9cd7607c75e5f3d40369a
Vaibhav Agrawal <vaibhav.ag464@gmail.com>
2023-07-09 00:27:42 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { Component } from 'react'
+import NavBar from './components/NavBar';
+import NewsBox from './components/NewsBox';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
-export default App;
+export default class App extends Component {
+  pageSize = 6;
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <NavBar />
+          <Routes>
+            <Route path='/' element={<NewsBox key='general' pageSize={this.pageSize} country="in" category="general" />}></Route>
+            <Route path='/business' element={<NewsBox key='business' pageSize={this.pageSize} country="in" category="business" />}></Route>
+            <Route path='/entertainment' element={<NewsBox key='entertainment' pageSize={this.pageSize} country="in" category="entertainment" />}></Route>
+            <Route path='/general' element={<NewsBox key='general' pageSize={this.pageSize} country="in" category="general" />}></Route>
+            <Route path='/health' element={<NewsBox key='health' pageSize={this.pageSize} country="in" category="health" />}></Route>
+            <Route path='/science' element={<NewsBox key='science' pageSize={this.pageSize} country="in" category="science" />}></Route>
+            <Route path='/sports' element={<NewsBox key='sports' pageSize={this.pageSize} country="in" category="sports" />}></Route>
+            <Route path='/technology' element={<NewsBox key='technology' pageSize={this.pageSize} country="in" category="technology" />}></Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
+    )
+  }
+}
