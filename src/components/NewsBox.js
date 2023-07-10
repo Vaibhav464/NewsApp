366a69a6e637e8e734a837f9ea0f579f6b9480bc
Vaibhav Agrawal <vaibhav.ag464@gmail.com>
2023-07-10 18:58:58 +0530

    Added Infinte scroll bar

--- src/components/NewsBox.js
+++ src/components/NewsBox.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export class News extends Component {
   static defaultProps = {
@@ -16,7 +17,7 @@ export class News extends Component {
     category: PropTypes.string
   }
 
-  capitalizeFirstLetter = (first)=>{
+  capitalizeFirstLetter = (first) => {
     return first.charAt(0).toUpperCase() + first.slice(1);
   }
 
@@ -25,7 +26,8 @@ export class News extends Component {
     this.state = {
       articles: [],
       loading: false,
-      page: 1
+      page: 1,
+      totalResults: 0
     }
     document.title = `${this.capitalizeFirstLetter(this.props.category)} News - NewsApp`
   }
@@ -35,39 +37,59 @@ export class News extends Component {
     this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json()
-    this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults, loading: false })
+    this.setState({ 
+      articles: parsedData.articles, 
+      totalResults: parsedData.totalResults, 
+      loading: false,
+    })
   }
   async componentDidMount() {
     this.updateNews();
   }
 
-  handlePrevClick = async () => {
-    this.setState({ page: this.state.page - 1 })
-    this.updateNews();
-  }
-
-  handleNextClick = async () => {
-    this.setState({ page: this.state.page + 1 })
-    this.updateNews();
-  }
+  fetchMoreData = async () => {
+    this.setState({page: this.state.page + 1});
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=59beeee78aec44d299ed71f1789328c7&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    this.setState({ 
+      articles: this.state.articles.concat(parsedData.articles), 
+      totalResults: parsedData.totalResults, 
+    })
+  };
 
   render() {
     return (
-      <div className='container my-3'>
+      <>
         <h1 className="text-center" style={{ margin: '30px 0px' }}>Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
         {this.state.loading && <Spinner />}
-        <div className="row">
-          {!this.state.loading && this.state.articles.map((element) => {
-            return <div className="col-md-4" key={element.url}>
-              <NewsItem title={element.title ? element.title.slice(0, 40) : ""} description={element.description ? element.description.slice(0, 80) : ""} imageUrl={element.urlToImage} newsUrl={element.url} date={element.publishedAt} author={element.author} source={element.source.name} />
-            </div>
-          })}
-        </div>
-        <div className="container d-flex justify-content-between">
-          <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous </button>
-          <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}> Next &rarr; </button>
-        </div>
-      </div>
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner/>}
+        >
+          <div className="container">
+          <div className="row">
+            {this.state.articles.map((element) => {
+              return <div className="col-md-4" key={element.url}>
+                <NewsItem title={element.title ? element.title.slice(0, 40) : ""} 
+                description={element.description ? element.description.slice(0, 80) : ""} 
+                imageUrl={element.urlToImage} 
+                newsUrl={element.url} 
+                date={element.publishedAt} 
+                author={element.author} 
+                source={element.source.name} />
+              </div>
+            })}
+          </div>
+          </div>
+        </InfiniteScroll>
+        </>
+        // {/* <div className="container d-flex justify-content-between">
+        //   <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous </button>
+        //   <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}> Next &rarr; </button>
+        // </div> */}
     )
   }
 }
