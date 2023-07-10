4d37de6713eb6b57a74891df1834c6b843566ef1
Vaibhav Agrawal <vaibhav.ag464@gmail.com>
2023-07-11 00:06:03 +0530

    "Added react top loading bar"

--- src/App.js
+++ src/App.js
@@ -8,23 +8,38 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar'
 
 export default class App extends Component {
   pageSize = 6;
+  apiKey=process.env.REACT_APP_NEWS_API
+
+  state = {
+    progress:0
+  }
+  setProgress=(progress)=>{
+    this.setState({progress: progress})
+  }
+
   render() {
     return (
       <BrowserRouter>
         <div>
+        <LoadingBar
+        color='#f11946'
+        height={3}
+        progress={this.state.progress}
+      />
           <NavBar />
           <Routes>
-            <Route path='/' element={<NewsBox key='general' pageSize={this.pageSize} country="in" category="general" />}></Route>
-            <Route path='/business' element={<NewsBox key='business' pageSize={this.pageSize} country="in" category="business" />}></Route>
-            <Route path='/entertainment' element={<NewsBox key='entertainment' pageSize={this.pageSize} country="in" category="entertainment" />}></Route>
-            <Route path='/general' element={<NewsBox key='general' pageSize={this.pageSize} country="in" category="general" />}></Route>
-            <Route path='/health' element={<NewsBox key='health' pageSize={this.pageSize} country="in" category="health" />}></Route>
-            <Route path='/science' element={<NewsBox key='science' pageSize={this.pageSize} country="in" category="science" />}></Route>
-            <Route path='/sports' element={<NewsBox key='sports' pageSize={this.pageSize} country="in" category="sports" />}></Route>
-            <Route path='/technology' element={<NewsBox key='technology' pageSize={this.pageSize} country="in" category="technology" />}></Route>
+            <Route path='/' element={<NewsBox setProgress={this.setProgress} apiKey={this.apiKey} key='general' pageSize={this.pageSize} country="in" category="general" />}></Route>
+            <Route path='/business' element={<NewsBox setProgress={this.setProgress} apiKey={this.apiKey} key='business' pageSize={this.pageSize} country="in" category="business" />}></Route>
+            <Route path='/entertainment' element={<NewsBox setProgress={this.setProgress} apiKey={this.apiKey} key='entertainment' pageSize={this.pageSize} country="in" category="entertainment" />}></Route>
+            <Route path='/general' element={<NewsBox setProgress={this.setProgress} apiKey={this.apiKey} key='general' pageSize={this.pageSize} country="in" category="general" />}></Route>
+            <Route path='/health' element={<NewsBox setProgress={this.setProgress} apiKey={this.apiKey} key='health' pageSize={this.pageSize} country="in" category="health" />}></Route>
+            <Route path='/science' element={<NewsBox setProgress={this.setProgress} apiKey={this.apiKey} key='science' pageSize={this.pageSize} country="in" category="science" />}></Route>
+            <Route path='/sports' element={<NewsBox setProgress={this.setProgress} apiKey={this.apiKey} key='sports' pageSize={this.pageSize} country="in" category="sports" />}></Route>
+            <Route path='/technology' element={<NewsBox setProgress={this.setProgress} apiKey={this.apiKey} key='technology' pageSize={this.pageSize} country="in" category="technology" />}></Route>
           </Routes>
         </div>
       </BrowserRouter>
